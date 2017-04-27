c68391e54f3e78fa898ab09582d1fa7f2fe23659
Zheng,Ming-Hong <free576002@gmail.com>
2017-04-27 21:38:13 +0800

    20170427

--- GCM_Sender/Controllers/GCM_ViewController.cs
+++ GCM_Sender/Controllers/GCM_ViewController.cs
@@ -5,14 +5,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GCM_Sender_Service;
+using KeyValue.Models;
 
 namespace GCM_Sender.Controllers
 {
     public class GCM_ViewController : Controller
     {
-
+        Sender _GCM_Sender_Service = new Sender();
         GetDBFunction GetDB = new GetDBFunction();
         gcmModels _ShowViewModels = new gcmModels();
+        gcmKeyValue _gcmKeyValue = new gcmKeyValue();
         // GET: GCM_View
         public ActionResult UserPage()
         {
@@ -21,15 +24,16 @@ namespace GCM_Sender.Controllers
         }
 
         [HttpPost]
-        public ActionResult ToDoSender(string gcm_user,string gcm_message)
+        public ActionResult ToDoSender(List<string> gcm_user,string gcm_message)
         {
+            bool sender = _GCM_Sender_Service.SendMessage(_gcmKeyValue.API_Key, gcm_message, gcm_user);
             var returnData = new { IsSuccess = false, Message = "" };
-            if (true)
+            if (sender)
             {//傳送成功
                 returnData = new
                 {
                     IsSuccess = true,
-                    Message = string.Format("已將訊息 {0} 傳送給使用者 {1}", gcm_message, GetDB.getUserName(gcm_user))
+                    Message = string.Format("已傳送給使用者")
                 };
             }
             else
@@ -37,7 +41,7 @@ namespace GCM_Sender.Controllers
                 returnData = new
                 {
                     IsSuccess = false,
-                    Message = string.Format("無法傳送訊息給 {0}", GetDB.getUserName(gcm_user))
+                    Message = string.Format("無法傳送訊息")
                 };
             }
             return Json(returnData);
---
+++ GCM_Sender/Models/KeyValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KeyValue.Models
+{
+    public class gcmKeyValue
+    {
+        public string API_Key = "AIz...";
+
+    }
+}
